1270af2fdd651af479b8d0c21ed8b5aaeb9ee0b1
Zibeon Jonriano Wisnumoerti <zibeonjonriano21@gmail.com>
2025-10-25 09:49:39 +0700

    penyesuaian req dan folder lapangin2

--- lapangin2/urls.py
+++ lapangin2/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for lapangin project.
+URL configuration for lapangin2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
