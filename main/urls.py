0a6e22fc882aea57a33aa0158270c85e77ef4370
Syifa <syifaakrt@gmail.com>
2025-11-30 12:18:09 +0700

    landing views

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # Path kosong: Ini akan menjadi homepage /
     path('', views.show_landing_page, name='home'),
     path('proxy-image/', views.proxy_image, name='proxy_image'),
+    path('api/booking/', views.get_lapangan_list, name='get_booking_list'),
 ]
