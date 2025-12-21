fa6823e5750ffe09a86efd104c7705b2e148d6c3
Zibeon Jonriano Wisnumoerti <zibeonjonriano21@gmail.com>
2025-12-21 07:35:59 +0700

    mengembalikan kode settings.py dan edit fungsi show_landing_page di main/views.py

--- lapangin2/settings.py
+++ lapangin2/settings.py
@@ -58,10 +58,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+
     'authbooking.middleware.DebugRequestMiddleware',  #untuk debugging authbooking
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -154,16 +155,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-if not PRODUCTION:  # atau if DEBUG:
+if DEBUG:
     STATICFILES_DIRS = [
         BASE_DIR / 'static' # merujuk ke /static root project pada mode development
     ]
-# else:
-#     STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
+else:
+    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Di bagian bawah file, setelah STATIC_ROOT
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
